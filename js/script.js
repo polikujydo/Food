b5dc4e96ab31ad6688b49cb4534d7e53d87615b6
Michael Benett <michaelbenett@Michaels-MacBook-Pro.local>
2023-05-29 17:30:27 +0300

    tabs

---
+++ js/script.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', () => {
+    // TABS
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsParent = document.querySelector('.tabheader__items'),
+          tabsContent = document.querySelectorAll('.tabcontent');
+
+//Hide tabsContent Func
+    function hideTabContent(){
+        tabsContent.forEach(eachTabContent => {
+            eachTabContent.classList.add('hide'),
+            eachTabContent.classList.remove('show', 'fade')
+        })
+        tabs.forEach(eachTab => {
+            eachTab.classList.remove('tabheader__item_active')
+        })
+    }
+
+//Show tabsContent Func
+    function showTabContent(i = 0){
+        tabsContent[i].classList.add('show', 'fade'),
+        tabsContent[i].classList.remove('hide')
+        tabs[i].classList.add('tabheader__item_active')
+    }
+    hideTabContent()
+    showTabContent()
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target
+        if (target && target.classList.contains('tabheader__item')){
+            tabs.forEach((eachTab, i) => {
+                if (target == eachTab){
+                    hideTabContent()
+                    showTabContent(i)
+                }
+            })
+        }
+    })
+})
---
+++ readme.md
@@ -0,0 +1,1 @@
+1. To addEventListener on tabs and change content while switch them(adding active class)
