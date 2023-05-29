7fb4c0d5b9d9fea57fd1f32c6007a1ff63b1e2af
Michael Benett <michaelbenett@Michaels-MacBook-Pro.local>
2023-05-29 18:33:05 +0300

    timer

--- js/script.js
+++ js/script.js
@@ -73,4 +73,59 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     })
 
+    // TIMER
+
+    const deadLine = '2024-01-01';
+
+    //deadLine & currentTime offset Func
+    function timeRemaining(deadLine){
+        const totalTime = Date.parse(deadLine) - Date.parse(new Date()), //ms to deadLine - current time
+              days = Math.floor(totalTime / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((totalTime / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((totalTime / (1000 * 60) % 60)),
+              seconds = Math.floor((totalTime / 1000) % 60);
+        return {
+            'total': totalTime,
+            'days': days,
+            "hours": hours,
+            "minutes": minutes,
+            "seeconds": seconds
+        }
+    }
+
+    function getZero(num){
+        if(num >= 0 && num < 10){
+            return `0${num}`
+        } else{
+           return num
+        }
+    }
+
+    function setClock(selector, deadLine){
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+              
+        updateClock();
+        
+        function updateClock(){
+            const totalTime = timeRemaining(deadLine)
+
+            days.innerText = getZero(totalTime.days)
+            hours.innerText = getZero(totalTime.hours)
+            minutes.innerText = getZero(totalTime.minutes)
+            seconds.innerText = getZero(totalTime.seeconds)
+
+            if (totalTime.total <= 0){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadLine)
+
+
 })
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
-1. To addEventListener on tabs and change content while switch them(adding active class)
+1. To addEventListener on tabs and change content while switch them(adding active class);
+2. Show modal after the click, it has to closing after clicking on body, escape and closeModal 
+3. To set a Clock with 0 before nums that loser than 10
