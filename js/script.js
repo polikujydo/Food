3c5da0967291b72edb5f5848e86e04b7360aa6d6
Michael Benett <michaelbenett@Michaels-MacBook-Pro.local>
2023-05-30 17:54:24 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -77,7 +77,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const deadLine = '2024-01-01';
 
-    //deadLine & currentTime offset Func
+//deadLine & currentTime offset Func
     function timeRemaining(deadLine){
         const totalTime = Date.parse(deadLine) - Date.parse(new Date()), //ms to deadLine - current time
               days = Math.floor(totalTime / (1000 * 60 * 60 * 24)),
@@ -127,5 +127,82 @@ window.addEventListener('DOMContentLoaded', () => {
 
     setClock('.timer', deadLine)
 
+    // SLIDER
+
+    let offset = 0;
+    let slideIndex = 1;
+
+    const slides = document.querySelectorAll('.offer__slide'),
+          prevArrow = document.querySelector('.offer__slider-prev'),
+          nextArrow = document.querySelector('.offer__slider-next'),
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current'),
+          slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+          slidesWidth = window.getComputedStyle(slidesWrapper).width,
+          slidesInner = document.querySelector('.offer__slider-inner');
+
+//Nums
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+        current.textContent =  `0${slideIndex}`;
+    } else {
+        total.textContent = slides.length;
+        current.textContent =  slideIndex;
+    }
+    
+    slidesInner.style.width = 100 * slides.length + '%';
+    slidesInner.style.display = 'flex';
+    slidesInner.style.transition = '0.5s all';
+
+    slidesWrapper.style.overflow = 'hidden';
+
+    slides.forEach(slide => {
+        slide.style.width = slidesWidth;
+    });
+
+//Arrows
+    nextArrow.addEventListener('click', () => {
+        if (offset == (+slidesWidth.slice(0, slidesWidth.length - 2) * (slides.length - 1))) {
+            offset = 0;
+        } else {
+            offset += +slidesWidth.slice(0, slidesWidth.length - 2); 
+        }
+
+        slidesInner.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+    });
+
+    prevArrow.addEventListener('click', () => {
+        if (offset == 0) {
+            offset = +slidesWidth.slice(0, slidesWidth.length - 2) * (slides.length - 1);
+        } else {
+            offset -= +slidesWidth.slice(0, slidesWidth.length - 2);
+        }
+
+        slidesInner.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = eachSlide.length;
+        } else {
+            slideIndex--;
+        }
+
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+    });
 
 })
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 1. To addEventListener on tabs and change content while switch them(adding active class);
 2. Show modal after the click, it has to closing after clicking on body, escape and closeModal 
 3. To set a Clock with 0 before nums that loser than 10
+4. To make slider horizontal slide:3 
